1eb2f485a2905d41eb645f43d08adb7fb68b3f61
Akash-Ugale <akashdugale2003@gmail.com>
2025-03-09 02:02:57 +0530

    CORS enabled

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const app = express();
 
 app.use(
     cors({
-      origin: ["http://localhost:3000", "http://localhost:5173"],
+      origin: ["http://localhost:3000", "http://localhost:5173", "https://the-recipe-haven.netlify.app"],
       credentials: true,
     })
   );
-  
+
 // Middleware
 app.use(express.json());
 
